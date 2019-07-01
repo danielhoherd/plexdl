0fa1f6c895c81599780fb3ad569bcaf22f2d5207
Daniel Hoherd <daniel.hoherd@gmail.com>
2019-07-01 11:16:55 -0700

    Use pip3. s/Pipfile.lock/.requirements/

--- pipenv.mk
+++ pipenv.mk
@@ -2,11 +2,12 @@
 pipenv-clean: ## Destroy pipenv virtual environment
 	pipenv --rm || true
 	rm -f Pipfile.lock || true
+	rm -f .requirements || true
 
 .PHONY: pipenv
-pipenv: Pipfile.lock ## Initialize pipenv virtual environment
-Pipfile.lock:
-	pip3 install --user --upgrade pipenv || \
-	pip install --user --upgrade pipenv
+pipenv: .requirements ## Initialize pipenv virtual environment
+.requirements:
+	pip3 install --user --upgrade pipenv
 	pipenv --three
 	pipenv install
+	touch .requirements
