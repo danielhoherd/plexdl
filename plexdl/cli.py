a9f1e84529034f86e88cc92bcc61da8e6d4ebeb6
Daniel Hoherd <daniel.hoherd@gmail.com>
2019-08-07 17:32:11 -0700

    Add version flag, fix error handling and main flow.

--- plexdl/cli.py
+++ plexdl/cli.py
@@ -5,6 +5,7 @@ import sys
 import plexapi
 from click import argument
 from click import command
+from click import echo
 from click import option
 
 import plexdl
@@ -17,6 +18,13 @@ def get_logger(ctx, param, value):
     return value
 
 
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    echo("0.1.0")
+    ctx.exit()
+
+
 @command()
 @option("-v", count=True, help="Increase verbosity (max -vvvv)", callback=get_logger)
 @option("-u", "--username", help="Your Plex username (env PLEXDL_USER)", envvar="PLEXDL_USER")
@@ -27,29 +35,27 @@ def get_logger(ctx, param, value):
 @option("--summary/--no-summary", default=False, help="Output summary about each result")
 @option("--ratings/--no-ratings", default=False, help="Output rating information for each result")
 @option("--metadata/--no-metadata", default=False, help="Output media metadata about each file for each result")
+@option("--version", is_flag=True, callback=print_version, expose_value=False, is_eager=True, help="Print version and exit")
 @argument("title", envvar="PLEXDL_TITLE")
 def main(username, password, title, relay, server_info, item_prefix, summary, ratings, metadata, v):
     """Searches your plex account for media matching the given string, then prints out download commands."""
 
-    p = plexdl.Client(
-        username=username,
-        password=password,
-        title=title,
-        relay=relay,
-        debug=v,
-        item_prefix=item_prefix,
-        server_info=server_info,
-        summary=summary,
-        ratings=ratings,
-        metadata=metadata,
-    )
     try:
+        p = plexdl.Client(
+            username=username,
+            password=password,
+            title=title,
+            relay=relay,
+            debug=v,
+            item_prefix=item_prefix,
+            server_info=server_info,
+            summary=summary,
+            ratings=ratings,
+            metadata=metadata,
+        )
+
         p.main()
     except KeyboardInterrupt:
         sys.exit(1)
     except plexapi.exceptions.BadRequest:
         sys.exit(2)
-
-
-if __name__ == "__main__":
-    main()
